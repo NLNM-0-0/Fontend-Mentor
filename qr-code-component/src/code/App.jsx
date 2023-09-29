682c212f770b9864fc6c8fdd668b40354d95d32b
Mai <113424382+NLNM-0-0@users.noreply.github.com>
2023-09-29 13:22:09 +0700

    dev: init qr code component challenge

---
+++ qr-code-component/src/code/App.jsx
@@ -0,0 +1,21 @@
+function App() {
+  return (
+    <div className="bg-white rounded-[16px] m-4 p-4 max-w-[400px] shadow-lg">
+      <img
+        className="max-w-[100%] rounded-[inherit] mb-4"
+        src={require("../images/image-qr-code.png")}
+      />
+      <div className="p-2">
+        <h1 className="lg:text-3xl">
+          Improve your front-end skills by building project
+        </h1>
+        <p className="lg:text-xl">
+          Scan the QR code to visit Frontend Mentor and take your coding skills
+          to the next level
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
